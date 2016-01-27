3ab28f00a3e05c77139259f3e311616f73da1b65
wellington-barbosa-daitan <Wellington@WELLINGTONs-MacBook-Pro.local>
2016-01-27 02:41:16 -0200

    Added DrawerLayout

--- app/src/main/java/com/wellingtonmb88/aprovado/activity/MainActivity.java
+++ app/src/main/java/com/wellingtonmb88/aprovado/activity/MainActivity.java
@@ -1,10 +1,15 @@
 package com.wellingtonmb88.aprovado.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,7 +29,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends BaseActivity implements MainView {
+public class MainActivity extends BaseActivity implements MainView, NavigationView.OnNavigationItemSelectedListener {
 
     private static final int NUM_TABS = 2;
 
@@ -34,6 +39,10 @@ public class MainActivity extends BaseActivity implements MainView {
     TabLayout mTabs;
     @Bind(R.id.toolbar_layout)
     Toolbar mToolbarLayout;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @Bind(R.id.nav_view)
+    NavigationView mNavigationView;
 
     @Inject
     MainPresenterImpl mMainPresenter;
@@ -52,6 +61,7 @@ public class MainActivity extends BaseActivity implements MainView {
 
         mMainPresenter.registerView(this);
 
+
         CharSequence titles[] = {getString(R.string.tablebar_header_classes), getString(R.string.tablebar_header_calculator)};
         setViewPageAdapter(titles, NUM_TABS);
 
@@ -59,6 +69,23 @@ public class MainActivity extends BaseActivity implements MainView {
         setSupportActionBar(mToolbarLayout);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setLogo(R.mipmap.actionbar_approved_logo);
+        }
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, mToolbarLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
@@ -112,5 +139,25 @@ public class MainActivity extends BaseActivity implements MainView {
         Intent intent = new Intent(MainActivity.this, AboutActivity.class);
         intent.putExtra(Constants.TabSharedPreferences.SELECTED_TAB, mPager.getCurrentItem());
         startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_login) {
+        } else if (id == R.id.nav_sync_data) {
+        } else if (id == R.id.nav_feedback) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            String uriString =
+                    "market://details?id=com.wellingtonmb88.aprovado";
+            intent.setData(Uri.parse(uriString));
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
